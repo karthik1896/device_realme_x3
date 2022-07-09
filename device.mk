6a18d8cc0f9cb0808e96dbcdb5441cd812cc233a
Kyle Harrison <khwebmail@gmail.com>
2022-07-09 18:16:20 +0530

    x3:Don't write binary XML files
    - Android 12 now writes XML files in binary format by default. This can cause incompatibility with TWRP which can hang when attempting to read XML files e.g. /data/system/storage.xml
    
    - This commit sets the persist.sys.binary_xml property to false so that XML files are written in text format.
    
    Change-Id: If6430597d8459e62dd78aa458c58237c71618970
    
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- device.mk
+++ device.mk
@@ -127,6 +127,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/oplus
 
+# System
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
