a814b0743444e35e6727ea24d1a5dcee9e720968
karthik1896 <karthik1896@outlook.com>
2022-08-27 14:04:43 +0530

    x3:Adapt to Voltage OS
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- voltage_x3.mk
+++ voltage_x3.mk
@@ -21,18 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/realme/x3/device.mk)
 
-ifneq ($(VANILLA_BUILD),true)
-# Inherit from goolag
-$(call inherit-product, vendor/google/gms/config.mk)
-$(call inherit-product, vendor/google/pixel/config.mk)
-$(call inherit-product, vendor/gprivate/gprivate.mk)
-$(call inherit-product, vendor/partner_modules/build/mainline_modules_s_flatten_apex.mk)
-else
-$(warning Building vanilla)
-endif
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_x3
+PRODUCT_NAME := voltage_x3
 PRODUCT_DEVICE := x3
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := X3
