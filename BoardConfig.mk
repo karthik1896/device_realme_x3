a44a486ef7aa14e227d8017228fba63da6bd1e7c
Sal Savage <salsavage@google.com>
2022-08-28 13:27:15 +0530

    x3:Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is
    correct
    
    Change-Id: I07e83ca352ff969be0e53a0f63b6c940b6c34dfb
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,6 +152,7 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/vendor_overlay/etc/fstab.qcom
